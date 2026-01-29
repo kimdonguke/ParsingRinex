c85342522d52fb21865582381298e9b5a81f4105
kimdonguke <rlaehddnr100@gmail.com>
2026-01-29 17:11:51 +0900

    [Feat] OBS 파싱 함수 작성

--- Parsing_ex.m
+++ Parsing_ex.m
@@ -10,6 +10,7 @@ load('Obs_1hour.mat')
 [obs_data, obs_header] = rinexread("YONS00KOR_R_20253590000_01D_30S_MO.rnx");
 
 retrun_NAV = parsingNavigationBody(nav_data);
+return_OBS = parsingObsBody(obs_data);
 
 %% Navigation Parsing %%
 %======================%
@@ -17,8 +18,6 @@ retrun_NAV = parsingNavigationBody(nav_data);
 %Input : Rinex file
 %Output : (PRN * 1) cell
 %======================%
-
-
 function [return_NAV] = parsingNavigationBody(nav_data)
     navTable = nav_data.GPS;
     navTable=timetable2table(navTable);
@@ -26,16 +25,120 @@ function [return_NAV] = parsingNavigationBody(nav_data)
     navTable = convertToRtkLibFormat(navTable);
     return_NAV = groupingTable(navTable,'PRN');
 end
+
+
 function [return_NAV] = parsingNavigationHeader(nav_header)
 
 end
 
+%% Table grouping %%
+%======================%
+% grouping Table by specific(parameter) column(satellite ID)
+%Input : Table class, colum name(string)
+%Output : (total Group Number * 1) cell
+%======================%
 function [group] = groupingTable(dataTable, varName)
     parameterGroup=findgroups(dataTable.(varName));
     num_group=length(unique(parameterGroup));
     group = cell(num_group,1);
     for i=1:num_group
         group{i}=dataTable(parameterGroup==i,:);
+    end
+end
+
+function flatObsTable = parsingObsBody(rawObs)
+    rawObs = timetable2table(rawObs.GPS);
+    % parseObsToFlatTable
+    % rinexread 결과를 학습 및 MPC 계산용 Flat Table로 변환
+    %
+    % Output Columns:
+    % Time(Unix), PRN, P1, L1, S1, LLI1, P2, L2, S2, LLI2, P5, L5, S5, LLI5
+    
+    % 데이터 행 개수
+    numRows = height(rawObs);
+    
+    % -------------------------------------------------------
+    % 2. 기본 정보 (Time, PRN) 변환
+    % -------------------------------------------------------
+    
+    % Time: datetime -> Unix Timestamp (double)
+    % TimeZone이 없으면 UTC로 가정하여 변환 (로컬 시간 간섭 방지)
+    dt = rawObs.Time;
+    if isempty(dt.TimeZone)
+        dt.TimeZone = 'UTC';
+    end
+    Time = posixtime(dt);
+    
+    % PRN: SatelliteID (이미 숫자라고 하셨으므로 그대로 사용)
+    % 만약 categorical이나 cell이라면 double로 변환
+    if isnumeric(rawObs.SatelliteID)
+        PRN = double(rawObs.SatelliteID);
+    else
+        % 혹시 모를 상황 대비 (문자열 '1' -> 숫자 1)
+        PRN = double(string(rawObs.SatelliteID));
+    end
+    
+    % -------------------------------------------------------
+    % 3. L1 Frequency Data (C1C, L1C, S1C, LLI)
+    % -------------------------------------------------------
+    P1   = getCol(rawObs, 'C1C');       % Pseudorange
+    L1   = getCol(rawObs, 'L1C');       % Carrier Phase
+    SNR1   = getCol(rawObs, 'S1C');       % SNR (Signal Strength)
+    LLI1 = getCol(rawObs, 'L1C_LLI');   % Lock Loss Indicator
+    
+    % -------------------------------------------------------
+    % 4. L2 Frequency Data (Priority: W > X)
+    % -------------------------------------------------------
+    % C2W(Z-tracking)가 있으면 사용, 없으면 C2X(L2C) 사용
+    if ismember('C2W', rawObs.Properties.VariableNames)
+        P2   = getCol(rawObs, 'C2W');
+        L2   = getCol(rawObs, 'L2W');
+        SNR2   = getCol(rawObs, 'S2W');
+        LLI2 = getCol(rawObs, 'L2W_LLI');
+    else
+        P2   = getCol(rawObs, 'C2X');
+        L2   = getCol(rawObs, 'L2X');
+        SNR2   = getCol(rawObs, 'S2X');
+        LLI2 = getCol(rawObs, 'L2X_LLI');
+    end
+    
+    % -------------------------------------------------------
+    % 5. L5 Frequency Data (C5X) - 필요 시 사용
+    % -------------------------------------------------------
+    P5   = getCol(rawObs, 'C5X');
+    L5   = getCol(rawObs, 'L5X');
+    SNR5   = getCol(rawObs, 'S5X');
+    LLI5 = getCol(rawObs, 'L5X_LLI');
+    
+    % -------------------------------------------------------
+    % 6. 테이블 생성
+    % -------------------------------------------------------
+    flatObsTable = table(Time, PRN, ...
+        P1, L1, SNR1, LLI1, ...
+        P2, L2, SNR2, LLI2, ...
+        P5, L5, SNR5, LLI5);
+    
+    % (옵션) NaN 데이터 제거가 필요하면 여기서 수행
+    % flatObsTable = rmmissing(flatObsTable, 'DataVariables', {'P1', 'L1'});
+end
+
+% -------------------------------------------------------
+% 헬퍼 함수: 컬럼이 존재하면 가져오고, 없으면 NaN 벡터 반환
+% -------------------------------------------------------
+function val = getCol(tbl, varName)
+    if ismember(varName, tbl.Properties.VariableNames)
+        val = tbl.(varName);
+        % 혹시 데이터가 cell array로 되어있다면 double 변환
+        if iscell(val)
+            val = str2double(val);
+        end
+    else
+        val = nan(height(tbl), 1); % 컬럼 없으면 NaN으로 채움
+    end
+    
+    % rinexread가 간혹 LLI 등을 빈 값으로 읽을 때 NaN 처리
+    if ~isfloat(val) 
+        val = double(val);
     end
 end
 
